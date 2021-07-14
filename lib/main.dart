f2c34c2a7c62c662c51c433c79d6f4f668a5f0f2
Bruno Correia <bruninhocorreia2013@gmail.com>
2021-07-14 20:46:12 -0300

    client changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 // import 'package:bytebank/database/app_database.dart';
 // import 'package:bytebank/database/app_database.dart';
 // import 'package:bytebank/models/transferencia.dart';
+import 'package:bytebank/http/web_client.dart';
 import 'package:flutter/material.dart';
 import 'package:bytebank/screens/dashboard/dashboard.dart';
 // import 'models/contato.dart';
 
 void main() {
   runApp(BytebankApp());
+  findAll();
 }
 
 // saveTransfer(Transferencia(1, 100.0, 1000)).then((id) {
